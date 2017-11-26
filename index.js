d38acdac88e99067a3976a3fbdf06b040c46a333
Csikos, Balint <csikos.balint@fnf.hu>
2017-11-26 17:41:28 +0100

    'init'

---
+++ index.js
@@ -0,0 +1,25 @@
+const cheerio = require('cheerio');
+const https = require('https');
+const encodeUrl = require('encodeurl');
+
+https.get('https://thepiratebay.org/search/'+ encodeUrl(process.argv[2]) + '/0/99/0', (resp) => {
+  let data = '';
+ 
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+ 
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    const html = cheerio.load(data);
+    for ( let i = 0; i < html('#searchResult').find('.detLink').toArray().length; i++) {
+      console.log('https://thepiratebay.org' + html('#searchResult').find('.detLink').toArray()[i].attribs.href);
+      console.log(html('#searchResult').find('.detLink').toArray()[i].children[0].data);
+    }
+    
+  });
+ 
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
